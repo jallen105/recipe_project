dd910a6147d4f0f448ffb2655b7ec7bfd8c82c69
jallen105 <jonathanallen105@gmail.com>
2023-11-10 04:01:42 -0500

    created search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import React, {useState} from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
@@ -6,7 +7,21 @@ import RecipeData from './data.json'
 
 function App() {
 
-  const listRecipes = RecipeData.map((recipe, idx) => (
+  let listRecipes = RecipeData
+    const [searchInput, setSearchInput] = useState("")
+
+    const handleChange = (e) => {
+      e.preventDefault()
+      setSearchInput(e.target.value)
+    }
+
+    if (searchInput.length > 0) {
+      listRecipes = RecipeData.filter((recipe) => {
+        return recipe.Title.toLowerCase().match(searchInput)
+      })
+    }
+
+  listRecipes = listRecipes.map((recipe, idx) => (
     <Main key={idx} recipeData={recipe} />
   ))
 
@@ -15,6 +30,7 @@ function App() {
     <div>
       <Header />
       <main>
+        <input type='text' placeholder='Search here' onChange={handleChange} value={searchInput} />
         {listRecipes}
       </main>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Header = () => {
+  
+  
   return (
     <div>
         <h1>Christmas Recipes</h1>
-        <nav>navbar here</nav>
+        
     </div>
   )
 }
